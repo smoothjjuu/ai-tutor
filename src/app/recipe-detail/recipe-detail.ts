3f3a8b5d0b1a80fcd811bda8b7a8081d97a2db99
smooth.jjuu <smooth.jjuu@gmail.com>
2025-09-22 10:14:31 +0000

    Complete Phase 3, Module 11

--- src/app/recipe-detail/recipe-detail.ts
+++ src/app/recipe-detail/recipe-detail.ts
@@ -1,10 +1,9 @@
 import { Component, computed, input, signal } from '@angular/core';
 import { RecipeModel } from '../models';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [JsonPipe],
+  imports: [],
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css'
 })
