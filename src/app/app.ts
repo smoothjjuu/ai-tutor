926a5c7ff1a08f169a02f2e535e8716c41d3105c
smooth.jjuu <smooth.jjuu@gmail.com>
2025-09-22 07:24:43 +0000

    Complete Phase 2, Module 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,14 +5,14 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  protected readonly title = signal('Smart Recipe Box');
 }
