8252d0d2ef57f8915cef6293bb906ec33b90a0ec
smooth.jjuu <smooth.jjuu@gmail.com>
2025-09-22 12:09:39 +0000

    Complete Phase 3, Module 13

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,18 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { RecipeDetail } from "../recipe-detail/recipe-detail";
 import { MOCK_RECIPES } from '../mock-recipes';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule ],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css'
 })
 export class RecipeList {
   protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
-  MOCK_RECIPES: RecipeModel[] = MOCK_RECIPES;
-  
+  public MOCK_RECIPES: RecipeModel[] = MOCK_RECIPES;
+  public searchTerm = signal('');
+  public readonly filteredRecipes = computed(() => {
+    return this.MOCK_RECIPES.filter(recipe => recipe.name.toLowerCase().includes(this.searchTerm().toLowerCase()))
+  });
+
   protected setRecipe(recipe: RecipeModel): void {
     this.recipe.set(recipe);
   }
